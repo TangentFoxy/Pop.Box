00fe7f26171ab26167ceb383b20126da6c90d404
Paul Liverman III <paul.liverman.iii@gmail.com>
2015-11-30 11:55:52 -0800

    wipsshit

--- main.lua
+++ main.lua
@@ -1,12 +1,8 @@
 local pop = require "pop" --TODO tell user that pop must be required with SLASHES
 
 function love.load()
-    local box = pop.box() -- returns the box element
+    pop.box() -- returns the box element
     -- or pop.create("box") (this is what is actually called when you call pop.box())
-
-    --TODO uncomment these once text is implemented!
-    --local text = pop.text(box) -- box will become the parent element of text
-    --text:setText("Hello and welcome to an example of Pop.Box, a very small shitty example.")
 end
 
 function love.draw()
@@ -22,13 +18,7 @@ function love.mousereleased(button, x, y)
 end
 
 function love.keypressed(key)
-    if not pop.keypressed(key, unicode) then
-        if key == "escape" then
-            love.event.quit()
-        end
+    if key == "escape" then
+        love.event.quit()
     end
-end
-
-function love.keyreleased(key)
-    pop.keyreleased(key)
 end
